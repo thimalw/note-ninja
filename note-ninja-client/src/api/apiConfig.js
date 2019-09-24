d67abb8c3d01c87aca1d83d2881c005d2640cb2a
Thimal Wickremage <thimalmadushanka@gmail.com>
2019-09-24 15:05:36 +1000

    CSS and responsive bug fixes

--- note-ninja-client/src/api/apiConfig.js
+++ note-ninja-client/src/api/apiConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://api.thenote.space:80/api/v1';
-axios.defaults.baseURL = 'http://localhost:3000/api/v1';
+axios.defaults.baseURL = 'https://api.notes.thimal.me/api/v1';
+// axios.defaults.baseURL = 'http://localhost:3000/api/v1';
