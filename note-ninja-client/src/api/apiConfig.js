ebd811db6ac071278fe98b6c32e5436d846da1d7
Thimal Wickremage <thimalmadushanka@gmail.com>
2019-09-24 15:08:05 +1000

    remove debug code

--- note-ninja-client/src/api/apiConfig.js
+++ note-ninja-client/src/api/apiConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.notes.thimal.me/api/v1';
-// axios.defaults.baseURL = 'http://localhost:3000/api/v1';
+// axios.defaults.baseURL = 'https://api.notes.thimal.me/api/v1';
+axios.defaults.baseURL = 'http://localhost:3000/api/v1';
